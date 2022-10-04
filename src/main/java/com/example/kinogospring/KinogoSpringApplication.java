4c18c6b666cc46abe7e72c31bf9c3143a92ebe5e
Albert <albertnadoyan98@gmail.com>
2022-10-04 16:54:46 +0400

    Initial commit

---
+++ src/main/java/com/example/kinogospring/KinogoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinogospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinogoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinogoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kinogospring/KinogoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinogospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinogoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

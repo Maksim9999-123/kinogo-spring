255c6e4172b4c5682035d93b044f84691937507d
PC user <aslanyan.maksim@gmail.com>
2022-10-12 14:56:04 +0400

    admin_update

--- src/main/java/com/example/kinogospring/repository/CastCrewRepository.java
+++ src/main/java/com/example/kinogospring/repository/CastCrewRepository.java
@@ -3,7 +3,6 @@ package com.example.kinogospring.repository;
 import com.example.kinogospring.entity.CastCrew;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface CastCrewRepository extends JpaRepository<CastCrew,Integer> {
 

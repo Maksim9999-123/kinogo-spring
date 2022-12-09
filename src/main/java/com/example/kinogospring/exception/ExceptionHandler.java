a8de0b7b5e9f43c60a7e1f2122388414e7b57c2e
Albert <albertnadoyan98@gmail.com>
2022-12-09 14:25:57 +0400

    edit and update

--- src/main/java/com/example/kinogospring/exception/ExceptionHandler.java
+++ src/main/java/com/example/kinogospring/exception/ExceptionHandler.java
@@ -1,14 +1,9 @@
 package com.example.kinogospring.exception;
 
-import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @ControllerAdvice
 public class ExceptionHandler{
@@ -20,11 +15,6 @@ public class ExceptionHandler{
         return modelAndView;
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadRequestException.class)
-    public ModelAndView castAndCrewBadRequest(BadRequestException ex){
-        ModelAndView modelAndView = new ModelAndView("404");
-        modelAndView.addObject("exception", ex);
-        return modelAndView;
-    }
+
 
 }

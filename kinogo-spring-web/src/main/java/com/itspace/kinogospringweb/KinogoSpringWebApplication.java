9e5ae3b0b6e5b5006b3c68d757ea0a71bf8d2b7f
Albert <albertnadoyan98@gmail.com>
2022-12-11 14:35:29 +0400

    init mvc module

---
+++ kinogo-spring-web/src/main/java/com/itspace/kinogospringweb/KinogoSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.itspace.kinogospringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinogoSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinogoSpringWebApplication.class, args);
+    }
+
+}

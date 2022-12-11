b25fc572c59563b81da9bbc4200eb7f4005b3ab7
Albert <albertnadoyan98@gmail.com>
2022-12-11 14:33:27 +0400

    init rest module

---
+++ kinogo-spring-rest/src/main/java/com/itspace/kinogospringrest/KinogoSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.itspace.kinogospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinogoSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinogoSpringRestApplication.class, args);
+    }
+
+}
